839e9f5fc1579e3860bd03239b8e618436382e5e
Craig Lister <craig@listerhome.com>
2019-07-20 09:47:57 +1000

    Adding startup settings

--- PlantMoistureSensor.ino
+++ PlantMoistureSensor.ino
@@ -1,6 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
+int PIN_Sensor =      A0;
+int PIN_Power =       D7;
+int PIN_WarnLed =     D8;
 
+void setup() {
+  // Setup PIN Modes.
+  pinMode(PIN_Sensor, INPUT);
+  pinMode(PIN_Power, OUTPUT);
+  pinMode(PIN_WarnLed, OUTPUT);
+
+  // Turn off the things so we can save some power.
+  digitalWrite(PIN_Power, LOW);
+  digitalWrite(PIN_WarnLED, LOW);
+  
 }
 
 void loop() {
