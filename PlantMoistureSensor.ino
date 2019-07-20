3144a92318a5d4e30127cd9c28c5be8041b9cb25
Craig Lister <craig@listerhome.com>
2019-07-20 14:36:25 +1000

    Trying to read values using sub module

--- PlantMoistureSensor.ino
+++ PlantMoistureSensor.ino
@@ -1,40 +1,37 @@
-#include "ArduinoDeviceLibraries/FadingLED.h"
+#include "ArduinoDeviceLibraries/SoilSensor.h"
 
 int PIN_Sensor =      A0;
 int PIN_Power =       7;
 int PIN_WarnLED =     3;
 int readValue =       0;
 
+SoilSensor sensor;
+
 void setup() {
   
-  //Serial.begin(9600);
+  Serial.begin(9600);
   
   // Setup PIN Modes.
+
+  sensor.Init(PIN_Sensor, PIN_Power, 1000);
+  sensor.SetDebugMode(true);
   pinMode(PIN_Sensor, INPUT);
   pinMode(PIN_Power, OUTPUT);
   pinMode(PIN_WarnLED, OUTPUT);
 
-  // Turn off the things so we can save some power.
-  digitalWrite(PIN_Power, LOW);
-  digitalWrite(PIN_WarnLED, LOW);
-
-
   // Packet Soil was 355
   // Jeremy was 570 to 590, with nearly enough water.
-  
-
-  
+ 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(PIN_Power, HIGH);
-  digitalWrite(PIN_WarnLED, HIGH);
-  delay(20);
-  digitalWrite(PIN_WarnLED, LOW);
-  readValue = analogRead(PIN_Sensor); 
-  //Serial.print("Soil read value: ");
-  //Serial.println(readValue);
-  digitalWrite(PIN_Power, LOW);
-  delay(2000);
+  int newVal = sensor.Update();
+  Serial.print("*");
+  if(newVal != readValue) {
+    readValue = newVal;
+    Serial.println("");
+    Serial.println(readValue);  
+  }
+  
 }
