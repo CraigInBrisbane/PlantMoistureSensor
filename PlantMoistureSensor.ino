dbca518a1bf74e5ff32a9a94b3708562d7bf072c
Craig Lister <craig@listerhome.com>
2019-07-21 18:21:47 +1000

    Added LCD

--- PlantMoistureSensor.ino
+++ PlantMoistureSensor.ino
@@ -1,4 +1,15 @@
 #include "ArduinoDeviceLibraries/SoilSensor.h"
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH  128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32  // OLED display height, in pixels
+#define OLED_RESET    -1  // Reset pin # (or -1 if sharing Arduino reset pin)
+
+// Create the display object.
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // Configurable settings.
 int   PIN_Sensor =        A0;     // PIN to which the Sensor Signal wire is connected.
@@ -6,7 +17,7 @@ int   PIN_Power =         6;      // PIN which we will power the sensor from. No
 int   PIN_WarnLED =       3;      // PIN to which the LED Indicator will connect. This will flash when the sensor is below a definded value.
 bool  DebugMode =         true;   // If Debug is enabled, we will be logging data to the serial port.
 int   PollingInterval =   5000;   // Time in milliseconds when the sensor will be polled for a reading.
-int   LowLevelReading =   550;    // The low value which will enable the warning LED. i.e. The low value for "Plant needs water!"
+int   LowLevelReading =   600;    // The low value which will enable the warning LED. i.e. The low value for "Plant needs water!"
 
 // Define the Soil Sensor and variable for the last value.
 SoilSensor                sensor;
@@ -18,6 +29,46 @@ void setup() {
 
   if(DebugMode)
     Serial.begin(9600);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }  
+  display.clearDisplay();
+
+  for(int i=0; i< 64; i++) {
+    display.clearDisplay();
+    display.drawPixel(64-i, 16, WHITE);
+    display.drawPixel(64+i, 16, WHITE);
+    display.display();
+  }
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(10, 5);
+  display.println("Sensor:");
+  display.setCursor(10, 20);
+  display.println("Power: ");
+  display.setCursor(85, 5);
+  display.println("LED: ");
+
+  display.setCursor(55, 5);
+  display.println(PIN_Sensor);
+  display.setCursor(55, 20);
+  display.println(PIN_Power);
+  display.setCursor(115, 5);
+  display.println(PIN_WarnLED);
+
+  
+  display.display();  
+  
+ // display.drawPixel(64, 16, WHITE);
+ 
+ // display.display();
+  delay(500); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
   
   sensor.Init(PIN_Sensor, PIN_Power, PollingInterval);
   sensor.SetDebugMode(DebugMode);
@@ -35,15 +86,27 @@ void loop() {
   // Check the sensor value.
   int newVal = sensor.Update();
   
- 
   if(newVal != -1) { // Sensor returns -1 if it thinks it doesn't need to read yet, or isn't setup.
-    Serial.println(IsWarningState);
     if(DebugMode) {
       Serial.print("Recieved value from sensor: ");
       Serial.println(newVal);
     }
 
-   if(newVal != SensorValue) {
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(48, 15);
+    // Display static text
+    display.println(newVal);
+
+    
+    display.setTextSize(1);
+    display.setCursor(105,1);
+    display.println(LowLevelReading);
+
+
+
+    if(newVal != SensorValue) {
       SensorValue = newVal;
     
       // Are we entering a warning state?    
@@ -64,6 +127,14 @@ void loop() {
         digitalWrite(PIN_WarnLED, LOW);  
       }   
     }
+
+    
+    if(IsWarningState) {  
+      display.setTextSize(1);
+      display.setCursor(1,1);
+      display.println("Warning!");   
+    }
+    
+    display.display(); 
   }
-  
 }
